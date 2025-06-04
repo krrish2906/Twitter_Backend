f1ffaab0e288b046ac37d1e82f09798a9357d871
krrish2906 <krrishbansal290605@gmail.com>
2025-06-04 19:46:49 +0530

    changes made in tweet repository

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -17,7 +17,21 @@ class TweetRepository extends CrudRepository {
     
     async getWithComments(id) {
         try {
-            const tweet = await Tweet.findById(id).populate({ path: 'comments' }).lean();
+            const tweet = await Tweet.findById(id).populate({
+                path: 'comments',
+                populate: {
+                    path: 'comments'
+                }
+            }).lean();
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getAll(offset, limit) {
+        try {
+            const tweet = await Tweet.find().skip(offset).limit(limit);
             return tweet;
         } catch (error) {
             console.log(error);
